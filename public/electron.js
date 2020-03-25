32afb8af81c65944672fe5285511d65438abe19f
divinepaul <31608502+divinepaul@users.noreply.github.com>
2020-03-25 22:16:55 +0530

    fixes and packages.

--- public/electron.js
+++ public/electron.js
@@ -7,10 +7,10 @@ const isDev = require("electron-is-dev");
 
 let mainWindow;
 
-require("update-electron-app")({
-  repo: "kitze/react-electron-example",
-  updateInterval: "1 hour"
-});
+// require("update-electron-app")({
+//   repo: "kitze/react-electron-example",
+//   updateInterval: "1 hour"
+// });
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 900, height: 680, webPreferences: { nodeIntegration: true }});
