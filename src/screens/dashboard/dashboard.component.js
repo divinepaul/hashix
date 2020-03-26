1ffcae47774663896dfb7acd5df8986624dba106
divinepaul <31608502+divinepaul@users.noreply.github.com>
2020-03-26 12:30:54 +0530

    file_picker

--- src/screens/dashboard/dashboard.component.js
+++ src/screens/dashboard/dashboard.component.js
@@ -1,9 +1,28 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
+import encryptService from '../../services/fileencrypt.services';
 
-export class DashBoard extends Component{
-    render(){
-        return(
-            <h1>Hashix</h1>
+const {dialog} = window.require('electron').remote;
+export class DashBoard extends Component {
+
+
+    componentDidMount() {
+
+    }
+    openFiles(){
+        
+        let files = dialog.showOpenDialogSync({
+            properties:["openFile","multiSelections"]
+
+        });
+        console.log(files);
+    }
+
+
+    render() {
+        return (
+            <button className="file-open-button" onClick={this.openFiles}>
+                <h1>open file</h1>
+            </button>
         );
     }
 }
