20b9f85fdf58fafea27ec2008e85c929e2197c8f
Paul Prince <paulprince2454@gmail.com>
2020-03-27 12:36:32 +0530

    done

--- src/services/fileencrypt.services.js
+++ src/services/fileencrypt.services.js
@@ -2,55 +2,52 @@ const crypto = require('crypto');
 const fs = require('fs')
 const zlib = require('zlib');
 
-class streamEncrypt {
-
-
-
-    checkFiles() {
-        const fileContents = fs.readFileSync('./file.txt');
-        if(fileContents) {
-            console.log("File has been sucessfully loaded");
-        } else {
-            console.log("File loading failed");
-        }
-    }
+class cipherEncryption {
 
     constructor() {
-        this.algorithm = 'aes-192-cbc';
-        this.password  = 'password';
-        this.files = null
-    }
-
-    generateKey() {
-        const key = crypto.scryptSync(this.password,'salt',24);
-        const buffer = 16;
-        const vector = 0;
-        const iv = Buffer.alloc(buffer,vector);
-        const cipher = crypto.createCipheriv(this.algorithm,key,iv);
-        this.generateStream(cipher);
-            
+        this.password  = null;
+        this.algorithm = null;
+        this.files     = null;
     }
 
     setFiles(files) {
         this.files = files;
         console.log(files);
+
     }
 
-    generateStream(cip) {
+    setPassword(password) {
+        this.password = password;        
 
-        const input = fs.createReadStream(file.txt);
-        const zip = zlib.createGzip();
+    }
+    setAlgorithm(algorithm) {
+        this.algorithm = algorithm;
+
+    }
+    encryptFiles(){
+        const key = crypto.scryptSync(this.password,'salt',24);
+        const buffer = 16;
+        const vector = 0;
+        const iv = Buffer.alloc(buffer,vector);
+        const cipher = crypto.createCipheriv(this.algorithm,key,iv);
+        const input = fs.createReadStream(this.files[0]);
         const output = fs.createWriteStream('file.enc');
-        input.pipe(zip).pipe(cip).pipe(output);
+        input.pipe(cipher).pipe(output);
 
     }
+    decryptFiles(){
 
-
-        
+    }
 }
-const stream = new streamEncrypt();
-stream.checkFiles();
-stream.generateKey();
+
+const stream = new cipherEncryption();
 stream.setFiles([
     "file.txt"
-]);
+])
+stream.setPassword('password');
+stream.setAlgorithm('aes-192-cbc')
+stream.encryptFiles();
+
+
+
+
